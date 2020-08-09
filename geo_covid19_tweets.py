c8a2fd5f40c37c4bdd7c40651d9e0986db6b3fe2
Apoorv Bhardwaj <bhardwaj.apoorvs@gmail.com>
2020-08-09 13:25:27 +0530

    Restructing file

--- geo_covid19_tweets.py
+++ geo_covid19_tweets.py
@@ -7,60 +7,82 @@ from tweets_files import TweetsFiles
 
 class GeoCovid19Tweets:
 
-    @staticmethod
-    def process_tweet_files():
-        cleaned_tweets_files_dir = 'cleaned_tweets_files'
+    def __init__(self):
+        self.cleaned_tweets_files_dir = 'cleaned_tweets_files'
 
-        tweets_collection_start_date = '2020-02-01'
-        tweets_collection_end_date = '2020-05-01'
-        date_format = '%Y-%m-%d'
+        self.tweet = TweetsFiles(self.cleaned_tweets_files_dir)
 
-        summary_file_name = 'summary.csv'
-        summary_fieldnames = ['file_name', 'total_tweets', 'india_specific_tweets', 'outside_india_tweets',
-                              'india_specific_tweets_with_full_text', 'india_specific_tweets_without_full_text',
-                              'india_full_text_tweet_india_users', 'india_full_text_tweet_outside_india_users', ]
+        self.summaries = []
+
+        self.summary_file_name = 'summary.csv'
+        self.summary_fieldnames = ['file_name', 'total_tweets', 'india_specific_tweets', 'outside_india_tweets',
+                                   'india_specific_tweets_with_full_text', 'india_specific_tweets_without_full_text',
+                                   'india_full_text_tweet_india_users', 'india_full_text_tweet_outside_india_users', ]
+
+    def process_date_range_files(self, tweets_start_date, tweets_end_date, date_format,
+                                 download_only_mode, ):
 
         tweets_file_base_url_for_date_files = 'https://crisisnlp.qcri.org/covid_data/en_geo_files'
-        tweets_file_base_url_for_misc_file = 'https://crisisnlp.qcri.org/covid_data/misc_updates'
         tweets_date_file_name_template = 'en_geo_{}'
-        misc_update_file = 'geo_place_update_11_6_2020'
-        download_only_mode = True
 
-        if not os.path.exists(summary_file_name):
-            with open(summary_file_name, 'a', newline='') as csvFile:
-                writer = csv.DictWriter(csvFile, fieldnames=summary_fieldnames)
-                writer.writeheader()
-
-        start_date = datetime.strptime(tweets_collection_start_date, date_format)
-        end_date = datetime.strptime(tweets_collection_end_date, date_format)
-
-        tweet = TweetsFiles(cleaned_tweets_files_dir)
-
-        summaries = []
+        start_date = datetime.strptime(tweets_start_date, date_format)
+        end_date = datetime.strptime(tweets_end_date, date_format)
 
         while start_date <= end_date:
             file_name = tweets_date_file_name_template.format(datetime.strftime(start_date, date_format))
-            summary_row_dict = tweet.process_tweet_file(tweets_file_base_url_for_date_files,
-                                                        file_name, download_only_mode)
+            summary_row_dict = self.tweet.process_tweet_file(tweets_file_base_url_for_date_files,
+                                                             file_name, download_only_mode)
             start_date = start_date + timedelta(days=1)
-            if bool(summary_row_dict):
-                summaries.append(summary_row_dict)
 
-        summary_row_dict = tweet.process_tweet_file(tweets_file_base_url_for_misc_file, misc_update_file,
-                                                    download_only_mode)
+            self.add_to_summary(summary_row_dict)
 
+    def process_misc_update_file(self, download_only_mode):
+        tweets_file_base_url_for_misc_file = 'https://crisisnlp.qcri.org/covid_data/misc_updates'
+
+        misc_update_file = 'geo_place_update_11_6_2020'
+
+        summary_row_dict = self.tweet.process_tweet_file(tweets_file_base_url_for_misc_file, misc_update_file,
+                                                         download_only_mode)
+        self.add_to_summary(summary_row_dict)
+
+    def add_to_summary(self, summary_row_dict):
         if bool(summary_row_dict):
-            summaries.append(summary_row_dict)
+            self.summaries.append(summary_row_dict)
 
-        if len(summaries) > 0:
-            with open(summary_file_name, 'a', newline='') as csvFile:
-                writer = csv.DictWriter(csvFile, fieldnames=summary_fieldnames)
-                for summary_row_dict in summaries:
+    def process_tweet_files(self, download_only_mode, start_date, end_date, date_format,
+                            process_misc_update_file=False):
+
+        if not os.path.exists(self.summary_file_name):
+            with open(self.summary_file_name, 'a', newline='') as csvFile:
+                writer = csv.DictWriter(csvFile, fieldnames=self.summary_fieldnames)
+                writer.writeheader()
+
+        self.process_date_range_files(start_date, end_date, date_format, download_only_mode)
+
+        if process_misc_update_file:
+            self.process_misc_update_file(download_only_mode)
+
+        if len(self.summaries) > 0:
+            with open(self.summary_file_name, 'a', newline='') as csvFile:
+                writer = csv.DictWriter(csvFile, fieldnames=self.summary_fieldnames)
+                for summary_row_dict in self.summaries:
                     writer.writerow(summary_row_dict)
 
-        tweet.cleanup()
+        self.tweet.cleanup()
+
+    def initiate_processing_tweet_files(self):
+        download_only_mode = True
+
+        tweets_start_date = '2020-02-01'
+        tweets_end_date = '2020-02-02'
+        date_format = '%Y-%m-%d'
+
+        process_misc_update_file = False
+
+        self.process_tweet_files(download_only_mode, tweets_start_date, tweets_end_date, date_format,
+                                 process_misc_update_file)
 
 
 if __name__ == '__main__':
     geo_tweets = GeoCovid19Tweets()
-    geo_tweets.process_tweet_files()
+    geo_tweets.initiate_processing_tweet_files()
